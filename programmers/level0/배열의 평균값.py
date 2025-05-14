af00edc33c763aabdc9ebff4eaba6b2801eccbfa
신동석 <96603945+dss1222@users.noreply.github.com>
2025-05-14 15:45:32 +0900

    feat: 배열의 평균값

---
+++ programmers/level0/배열의 평균값.py
@@ -0,0 +1,9 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120817
+def solution(numbers):
+    return sum(numbers) / len(numbers)
+
+# 테스트
+if __name__ == "__main__":
+    print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  # 5.5
+
+
