1692220ff86afa0d275cab66382d0aae67d26fde
신동석 <96603945+dss1222@users.noreply.github.com>
2025-05-15 14:02:08 +0900

    나이 출력

---
+++ programmers/level0/나이 출력.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120821
+def solution(age):
+    return 2022 - age + 1
+
+# 테스트
+if __name__ == "__main__":
+    print(solution(40))  # 1983
+    print(solution(23))  # 2000
+
