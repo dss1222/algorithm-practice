4cebd345cf39ed3362f6075e26eabe3485fd2f8d
신동석 <96603945+dss1222@users.noreply.github.com>
2025-05-18 11:23:04 +0900

    배열 회전시키기

---
+++ programmers/level0/배열 회전시키기.py
@@ -0,0 +1,7 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120844
+def solution(numbers, direction):
+    if direction == "right":
+        numbers.insert(0, numbers.pop())
+    else:
+        numbers.append(numbers.pop(0))
+    return numbers
