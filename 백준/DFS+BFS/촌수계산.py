8517cc022a78105064d194361796969d7a3f8783
신동석 <96603945+dss1222@users.noreply.github.com>
2025-06-12 14:21:15 +0900

    촌수 계산

--- 백준/DFS+BFS/촌수계산.py
+++ 백준/DFS+BFS/촌수계산.py
@@ -2,23 +2,27 @@ n = int(input())
 target1, target2 = map(int, input().split())
 m = int(input())
 
+graph = [[] for _ in range(n + 1)]
+
 for _ in range(m):
-    graph = [[] for _ in range(n + 1)]
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
     
 visited = [False] * (n + 1)
 answer = -1
 
-def dfs(target, finished_target, cnt):
+def dfs(v, target, cnt):
     global answer
-    visited[target] = True
+    visited[v] = True
     
-    if target == finished_target:
+    if v == target:
         answer = cnt
         return
     
     for next in graph[v]:
         if not visited[next]:
-            dfs(next, finished_target, cnt + 1)
+            dfs(next, target, cnt + 1)
             
 dfs(target1, target2, 0)
 print(answer)
